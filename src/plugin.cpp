d84d9332377dc1dc93db066d3d3034a674ebc640
Yash Rathore <yash030m@gmail.com>
2026-02-14 19:31:07 +0530

    changes

--- src/plugin.cpp
+++
@@ -1,9 +0,0 @@
-#include "plugin.h"
-#include <qqml.h>
-#include "wallpaperenginemodel.h"
-
-void WallpaperEnginePlugin::registerTypes(const char *uri) {
-    qmlRegisterType<WallpaperEngineModel>(uri, 1, 0, "WallpaperEngineModel");
-}
-
-
